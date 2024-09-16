96a057f07913c8024f11c11327888207d1aee213
ryo0705s <inspire.sasaki15@gmail.com>
2024-09-16 19:20:02 +0900

    オンオフ教材のコードに書き換え

--- index.py
+++ index.py
@@ -1,12 +1,21 @@
 import time
-import RPi.GPIO as GPIO
 
+#GPIOの初期設定
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(17, GPIO.OUT)
+#GPIO18を入力端子設定
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-  GPIO.output(17,1)
-  time.sleep(1)
-  GPIO.output(17,0)
-  time.sleep(1)
+    #スイッチ状態取得
+    sw_status = GPIO.input(18)
+
+    #画面出力
+    if sw_status == 0:
+        print('スイッチON!')
+    else:
+        print('')
+
+    #少し待つ
+    time.sleep(0.3)    
